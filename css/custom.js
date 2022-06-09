a2e45afbed9bff4bb6c7bfda62e69bc3e4dee96f
Wilber Jose Matus Gonzalez <wilberj1987@gmail.com>
2022-06-08 22:33:20 -0600

    ajustes de menu  profiles-ext/panel-profiles#41

--- css/custom.js
+++ css/custom.js
@@ -34,6 +34,25 @@ window.addEventListener("load", () => {
             Menu.className = "ui-g header-container menu-active"
         }        
     }
+    const btnNotifications = document.createElement("spam");
+    btnNotifications.className = "notificationsSpan";
+    btnNotifications.onclick = ()=>{
+        const nodes = document.querySelectorAll(".ui-menu.ui-menu-dynamic.ui-widget");
+        console.log(nodes);
+        nodes.forEach(node => {
+            if (node.id.includes("notificationsBTN_menu")) {
+                if (node.className.includes("menu-active")) { 
+                    node.style.display = "none"; 
+                    node.style.webkitTransform = "center top"                 
+                    node.className += " menu-active";
+                }else {
+                    node.style.display = "block";          
+                    node.className.replace(" menu-active","");
+                }  
+            }
+        });
+    }
+    MenuToggle.parentNode.append(btnNotifications);
     let scrollV = 0
     const MenuSticky = document.querySelector("#menu-sticky");    
     window.addEventListener("scroll", (e)=>{        
